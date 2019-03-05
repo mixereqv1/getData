e152681297266521b3eb3f823548235242ed0c4d
mixereqv1 <40582642+mixereqv1@users.noreply.github.com>
2019-03-05 11:33:20 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,51 @@
-const btnGetData = document.querySelector("#getData");
+const getDataBtn = document.querySelector('.getDataBtn');
 const list = document.querySelector('.list');
 
-
-btnGetData.addEventListener('click', function() {
+getDataBtn.addEventListener('click',function(){
     let xhr = new XMLHttpRequest();
-    //console.log(xhr);
 
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/photos1", true);
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos',true);
 
     xhr.send();
 
-    xhr.addEventListener('load', function() {
-        if(this.status == 200) {
+    xhr.addEventListener('load',function(){
+        if(this.status === 200){
             const wynik = xhr.response;
             const jsonWynik = JSON.parse(wynik);
 
-            jsonWynik.forEach(function(a) {
-                const liElement = document.createElement("li");
-
-                liElement.innerText = a.title;
-
-                list.appendChild(liElement);
+            jsonWynik.forEach( function(a) {
+                let pElement = document.createElement('p');
+                pElement.innerText = a.id + '. ' + a.title;
+                list.appendChild(pElement);
             });
         }
         else{
-            list.innerText = "Error 404 - nie znaleziono";
+            list.innerText = 'Status połączenia to ' + this.status;
         }
     })
 
-    xhr.addEventListener('error', function(){
-        console.log('error');
+    xhr.addEventListener('error',function(a){
+        list.innerText = 'Brak połączenia internetowego.';
     })
-});
+})
+
+// const getDataBtn = document.querySelector('.getDataBtn');
+// const list = document.querySelector('.list');
+
+// getDataBtn.addEventListener('click',function(){
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos',true);
+//     xhr.send();
+
+//     xhr.addEventListener('load',function(){
+//         const wynik = xhr.response;
+//         const jsonWynik = JSON.parse(wynik);
+
+//         jsonWynik.forEach( function(a) {
+//             let pElement = document.createElement('p');
+//             pElement.innerText = a.id + '. ' + a.title;
+//             list.appendChild(pElement);
+//         });
+//     })
+// })
